a0dad5d037b57e23e0fa92c3dd15b1693d016e4e
Bryan Braun <bbraun7@gmail.com>
2025-02-17 15:53:34 -0500

    test 3: reorder log statements
    test result:
      b.js:5 Uncaught ReferenceError: Cannot access 'a' before initialization
      at b.js:5:25

--- a.js
+++ a.js
@@ -1,6 +1,5 @@
 import { b } from './b.js';
 
-console.log('a.js: b:', b);
-
 export const a = 'A';
 
+console.log('a.js: b:', b);
--- b.js
+++ b.js
@@ -1,5 +1,6 @@
 import { a } from './a.js';
 
+export const b = 'B';
+
 console.log('b.js: a:', a);
 
-export const b = 'B';
